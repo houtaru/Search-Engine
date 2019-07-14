d603f6ad72bfa306317bd2444db8ae4f8d41fc84
The San Cao <caothesan@gmail.com>
2019-07-14 11:04:59 +0700

    update tokenizer

---
+++ src/utils.cpp
@@ -0,0 +1,17 @@
+#include <utils.h>
+
+std::vector < std::string > tokenizer(std::string text) {
+    std::vector <std::string> cur, result;
+    boost::split(cur, text, [](char c){ return c == ' '; });
+
+    for (auto it : cur) {
+        bool ok = true;
+        for (auto jt : std::vector <std::string> {"I", "a", "about", "an", "are", "as", "at", "be", "by", "com", "for", "from", "how", "in", "is", "it", "of", "on", "or", "that", "the", "this", "to", "was", "what", "when", "where", "who", "will", "with", "the", "www"})
+            if (it == jt) {
+                ok = false;
+                break;
+            }
+        if (ok) result.push_back(it);
+    }
+    return result;
+}
---
+++ src/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H_DEFINED
+#define UTILS_H_DEFINED
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+std::vector < std::string > tokenizer(std::string s);
+
+#endif
