4b280825e97addef0b8fcbd870d3dc5f75b06ed3
The San Cao <caothesan@gmail.com>
2019-07-18 16:40:25 +0700

    push auto suggestion

--- src/Trie.hpp
+++ src/Trie.hpp
@@ -3,31 +3,40 @@
 
 #include <vector>
 #include <map> 
-struct Node {
-    std::vector<Node *> child; 
-    std::map<int, int> distribution; // number word X appear in text[i]
-    Node();
-    Node(int nChild);
-    ~Node();
-};
 
 class Trie {
 private:
+    struct Node {
+        std::vector<Node *> child; 
+        std::map<int, int> distribution; // number word X appear in text[i]
+        Node();
+        Node(int nChild);
+        ~Node();
+    };
+
     Node * root;
     int nChild;
 public:
     Trie(int nChild);
     ~Trie();
+    
     // Import Trie from file
     void Import();
+    
     // Export Trie to file
     void Export();
+    
     // Insert word in text[id]
     void Insert(std::string word, int id);
+    
     // Return the number of word appear in text[id]
     int Search(std::string word, int id);
+    
     // Return vector cntAppear of word 
     std::map<int, int> Search(std::string word);
+
+    //Auto suggestion
+    std::vector < std::string > auto_suggestion(std::string text, int lim = 5);
 };
 
 #endif // __TRIE__
