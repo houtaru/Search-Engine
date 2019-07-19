bf30ea6af983deda993be1cc0164a38eb1da27e1
Khoa Nguyen <khoa3101@gmail.com>
2019-07-19 09:12:43 +0700

    Update stuff

--- src/Ranking.cpp
+++ src/Ranking.cpp
@@ -5,7 +5,7 @@ double Ranking::weight_query(Trie &trie, string term, int count) {
     map<int, int> df = trie.Search(term);
     int df_term = df.size();
 
-    return (1 + log(count * 1.0)) * log(3 * 1.0 / df_term);
+    return count * log(3 * 1.0 / df_term);
 }
 
 vector<int> Ranking::output(Trie &trie, vector<string> query, int k) {
@@ -28,16 +28,15 @@ vector<int> Ranking::output(Trie &trie, vector<string> query, int k) {
             length[j.first] += j.second * j.second * 1.0;
 
             //  Compute dot producgt of vector q and vector d
-            score[j.first] += w_tq * (1 + log(j.second * 1.0));
+            score[j.first] += w_tq * j.second * 1.0;
         }
     }
-    cout << endl;
     //  Divide the score[i] by the length of document i and push it to a Heap
     Heap heap;
     for (int i = 0; i < 3; ++i) {
-        length[i] = sqrt(length[i]);
-        if (length[i] > 0.00001)
-            score[i] /= length[i];
+        //length[i] = sqrt(length[i]);
+        //if (length[i] > 0.00001)
+            //score[i] /= length[i];
         if (score[i] > 0.00001)
             heap.insert(pair<double, int>(score[i], i));
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Trie.hpp>
 #include <Heap.hpp>
 #include <Ranking.hpp>
+#include <utils.h>
 
 
 int main() {
+    Trie trie(256);
+    for (int i = 0; i < 3; ++i) {
+        ifstream fin("Data/" + to_string(i) + ".txt");
+        vector<string> ss;
+        string sub;
+        while (fin >> sub)
+            ss.push_back(sub);
+
+        trie.AddText(i, ss);
+        fin.close();
+    }
+
+    string query_init;
+    getline(cin, query_init);
+    vector<string> query = tokenizer(query_init);
+
+
+    cout << endl;
+    Ranking ranking;
+    for (auto i : ranking.output(trie, query, 3))
+        cout << i << endl;
 
 
     return 0;
