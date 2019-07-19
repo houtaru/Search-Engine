180aa5d5aade416d1f00f9caa5bcb270f625b108
Khoa Nguyen <khoa3101@gmail.com>
2019-07-19 09:19:39 +0700

    Add main

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <Trie.hpp>
+#include "Ranking.hpp"
+#include "utils.h"
+#include <iostream>
+#include <fstream>
+#include <Heap.hpp>
+
+using namespace std;
+
+int main() {
+    double stime = clock();
+    Trie trie(256);
+    trie.Import();
+    cerr << "Importing TRIE takes " << (clock() - stime) / CLOCKS_PER_SEC * 1000 << "ms.\n";
+    stime = clock();
+    ifstream fin("TextData2/___index.txt");
+    string filename;
+    int cnt = 0;
+    int totalChar = 0;
+    while (getline(fin, filename)) {
+        if (trie.HasText(cnt)) {
+       //     std::cerr << "Have added " << cnt << "already\n";
+            ++cnt;
+            continue;
+        }
+        ifstream data("TextData2/" + filename);
+        string st;
+        string text;
+        while (getline(data, st)) {
+            for (char c : st) {
+                // if ('A' <= c && c <= 'Z') c += 32;
+                // if (('a' <= c && c <= 'z') || ('0' <= c && c <= '9') || c == ' ')
+                    text.push_back(c);
+            }
+            text.push_back(' '); 
+        }
+        trie.AddText(cnt, text);
+        totalChar += text.size();
+        // cerr << "Done file " << cnt << ' ' << text.size() << '\n'; 
+        ++cnt;
+    }
+    cerr << "Adding all texts takes " << (clock() - stime) / CLOCKS_PER_SEC * 1000 << "ms.\n";
+    stime = clock();
+    trie.Export();
+    cerr << "Exporting TRIE takes " << (clock() - stime) / CLOCKS_PER_SEC * 1000 << "ms.\n";
+    stime = clock();
+    // cerr << "totalChar : " << totalChar << '\n';
+    // for (auto x : trie.Search("VIETNAM")) {
+    //     cerr << x.first << ' ' << x.second << '\n';
+    // }
+    //exit(0);
+    string query;
+    getline(cin, query);
+    //trie.Export();
+    Ranking ranking;
+    vector<int> Result = ranking.output(trie, tokenizer(query), 3);
+    for (int x : Result) {
+        cout << x << ' ';
+    }
+}
