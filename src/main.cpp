4176c954bb444141074aba465be79033dc58cd88
Khoa Nguyen <khoa3101@gmail.com>
2019-07-24 15:23:56 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,6 @@
-#include <ncurses.h>
-#include <bits/stdc++.h>
-
-#include <Engine/Utils/String.hpp>
-
-using namespace std;
-
-// void view_document(vector <string> query, string file) {
-//     //clear screeb
-//     //clear_scr()
-    
-//     //TITLE
-
-//     ifstream fin("input.in");
-//     vector <string> content;
-//     string ss;
-//     while (getline(fin, ss))
-//       content.push_back(ss);
-// }
+#include <Frontend.hpp>
 
 int main() {
-    // initscr();
-    // noecho();
-    // cbreak();
-    // keypad(stdscr, TRUE);
-    // curs_set(0);    //  Set cursor invisible
-
-    // vector <string> query = {
-    //   "nam", "ngoi", "khoc"
-    // };
-    // view_document(query, "input.in");
-
-    string s; getline(cin, s, '\n');
-    vector <string> cur = String::split(String::to_lower(s));
-    for (auto it : cur) cout << it << "\n";
-    return 0;
+    Frontend frontend;
+    frontend.loading_scr();
 }
